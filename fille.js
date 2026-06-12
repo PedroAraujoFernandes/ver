7e757a3708b81cad900b9b3b00cff1999ed39f17
pedro <solua@gmail.com>
2026-06-12 17:09:26 -0300

    retoques finais

--- fille.js
+++ fille.js
@@ -1,28 +1,33 @@
 let texto = document.getElementById("texto");
 let troco = 0;
-class valor{
+
+
+class tempoDeVaga{
     constructor(valor){
         this.valor = valor;
     }
     
-    pagar(valor){
+    pagar(valor, troco){
         if(valor < 1 || valor === null){
+            texto.innerHTML = `o valor é invalido`;
             console.log("o valor é invalido");
             
         }else{
             if(valor >= 3){
-                   let troco = valor - 3;
-                texto.innerHTML = `o valor do troco é ${troco}, você tem uma hora e meia`;
-            }
-            else{
-                if(valor >= 1.75){
-                     let troco = valor - 1.75;
-                    console.log("o valor do troco é: " + troco + "R$ você tem acesso a uma hora");
+                   const troco = valor - 3;
+                   texto.innerHTML = `o valor do troco é ${troco.toFixed(2)}R$, você tem uma 120 minutos.`;
+    
                 }
                 else{
-                    if(valor >= 1){
-                       let troco = valor - 1;
-                        console.log("o valor do troco é: " + troco + "R$ você tem acesso a meia hora");
+                    if(valor >= 1.75){
+                        const troco = valor - 1.75;
+                        texto.innerHTML = `o valor do troco é ${troco.toFixed(2)}R$, você tem 60 minutos.`;
+                    }
+                    else{
+                        if(valor >= 1){
+                         const troco = valor - 1;
+                         console.log(troco);
+                        texto.innerHTML = `o valor do troco é ${troco.toFixed(2)}R$, você tem 30 minutos.`;
                     }
                 }
     }
@@ -30,15 +35,14 @@ class valor{
 }
 }
 
+
 function validar(){
-    let yoyo = new valor;
+    let yoyo = new tempoDeVaga;
     let valorInserido = document.getElementById("campo").value;
     valorInserido = Number(valorInserido);
-    
+
     yoyo.pagar(valorInserido);
-    texto.innerHTML = "o valor do troco é " + troco;
     
 }
-
 
 
